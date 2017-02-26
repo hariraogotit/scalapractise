cc174dd3efe34d3cbfeb4762440f62b054a3eeeb
Hari Rao <hari.rao@outlook.com>
2017-02-26 16:44:55 +0000

    Post fix notation added

---
+++ src/main/scala/Calculator.scala
@@ -0,0 +1,62 @@
+import java.beans.Expression
+
+import scala.collection.mutable
+
+/**
+  * Created by Hari Rao on 26/02/17.
+  */
+class Calculator {
+
+  def handleOperator(token: String, stack: mutable.Stack[Int]): Boolean = token match {
+
+    case "+" =>
+      val rhs = stack.pop()
+      val lhs = stack.pop()
+      stack.push(lhs + rhs)
+      true
+    case "*" =>
+      val rhs = stack.pop()
+      val lhs = stack.pop()
+      stack.push(lhs * rhs)
+      true
+    case "-" =>
+      val rhs = stack.pop()
+      val lhs = stack.pop()
+      stack.push(lhs - rhs)
+      true
+    case "/" =>
+      val rhs = stack.pop()
+      val lhs = stack.pop()
+      stack.push(lhs / rhs)
+      true
+    case _ => false
+  }
+
+  def handleNumber(token: String, stack: mutable.Stack[Int]): Boolean =  {
+    try{
+      stack.push(token.toInt)
+      true
+    }catch{
+      case _: NumberFormatException => false
+    }
+  }
+
+  def calculate(expression: String): Int = {
+      val stack = new mutable.Stack[Int]()
+
+      for(token: String <- expression.split(" ")){
+        if(!handleOperator(token,stack) && !handleNumber(token,stack)){
+          throw  new IllegalArgumentException("Invalid token " + token)
+        }
+      }
+    stack.pop()
+  }
+
+  def parsePostFix(expression: String): Unit = {
+    if(expression!=null){
+      println("Evaluated post fix " + calculate(expression))
+    }else{
+      throw new IllegalArgumentException("Invalid expression " + expression)
+    }
+  }
+}
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,58 @@
+/**
+  * Created by Hari Rao on 25/02/17.
+  */
+object Main  extends App {
+
+ parseTheFirstClassCreated()
+
+ parseException()
+
+ parseForComprehensions()
+
+ parseExpression()
+
+ private def parseForComprehensions() = {
+  var forComprehensions = new ForComprehensions
+
+  println(forComprehensions.simpleForComp(List(2, 3, 6)))
+
+  println(forComprehensions.forCompWithBraces())
+
+  println(forComprehensions.forCompWithGuard())
+ }
+
+ private def parseTheFirstClassCreated() = {
+  var firstScalaClass = new FirstScalaClass
+
+  println(firstScalaClass.add(1, 2))
+
+  println(firstScalaClass.fib(4))
+
+  println(firstScalaClass.monthName(2))
+
+  println(firstScalaClass.monthName(12))
+
+  println(firstScalaClass.daysInMonth(12))
+
+  println(firstScalaClass.whatIsIt(List("Hari", "Rao")))
+
+  println(firstScalaClass.variableBindingPattern(1))
+
+  println(firstScalaClass.whatIsItVariableBindingPattern(List(1, 2, 3)))
+ }
+
+ private def parseException() = {
+  var exceptionPracs = new ExceptionPracs
+
+  println(exceptionPracs.parseInt("12"))
+
+  println(exceptionPracs.parseIntWithOption("hello"))
+ }
+
+ private def parseExpression() = {
+   var calculator = new Calculator
+
+   calculator.parsePostFix("1 2 + 3 4 5 - - * 6 7 8 - * /")
+ }
+
+}
