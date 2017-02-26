ab442b3ba5de734ecd0c968fd3b2dd34f9cbea50
Hari Rao <hari.rao@outlook.com>
2017-02-26 19:46:56 +0000

    Called the companion object in main

--- src/main/scala/Lesson/Three/Main.scala
+++ src/main/scala/Lesson/Three/Main.scala
@@ -7,6 +7,8 @@ object Main {
 
   def main(args: Array[String]) : Unit ={
     val cookBook = new CookBook()
+
+    val companionObject = CompanionObject.make(List("Carrot","Beans"),List("Chop carrot and cabbage","Boil both"))
   }
 
 }
